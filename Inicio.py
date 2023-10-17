3ce4c1f3c6101293eed9785b3c75209baa6fae42
Wellington Facciolla Lopes <83545945+wfacciolla2@users.noreply.github.com>
2023-10-17 09:30:51 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-with open('style.css') as f:
+with open('styles.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 st.markdown(f'''
     # Ficha para atendimento
